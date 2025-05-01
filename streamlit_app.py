d401128c4eb6c68a5de66b93e82b8e439d110251
Mihnea Galeteanu <mihnea.galeteanu@reach.com>
2025-05-01 15:36:06 -0400

    My first push

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+conn = st.connection("snowflake")
+df = conn.query("SELECT * FROM application_c limit 10;", ttl="10m")
+for row in df.itertuples():
+    st.write(f"{row.ID}")
